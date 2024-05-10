ec90159471995f1373b38ab5863164e34ce62909
black <jaberhc2002@gmail.com>
2024-05-10 17:06:38 +0000

    cpp06 : declare_multiple_variables

---
+++ cpp06_declare_multiple_variables/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  int x = 10, y = 20, z = 40;
+  int a,b,c;
+  a = b = c = 50;
+  cout << x + y + z << endl;
+  cout << a + b + c ;
+  return 0;
+}
