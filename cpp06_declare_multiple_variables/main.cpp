5f9c9ed4b906095359b324fd3b7190df8c72c9b3
Black Light <78747748+JaberChowdhury@users.noreply.github.com>
2025-01-28 10:55:15 +0600

    add : implement for loop examples and nested loops in C++

--- cpp06_declare_multiple_variables/main.cpp
+++ cpp06_declare_multiple_variables/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int x = 10, y = 20, z = 40;
-  int a, b, c;
-  a = b = c = 50;
-  cout << x + y + z << endl;
-  cout << a + b + c;
+// int main() {
+//   int x = 10, y = 20, z = 40;
+//   int a, b, c;
+//   a = b = c = 50;
+//   cout << x + y + z << endl;
+//   cout << a + b + c;
+//   return 0;
+// }
+
+int main()
+{
+  for (int i = 0; i <= 10; i = i + 2)
+  {
+    cout << i << "\n";
+  }
   return 0;
 }
