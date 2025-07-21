901f9eff205d2dc62f3b507faac4249330a7f08d
black <jaberhc2002@gmail.com>
2025-07-22 04:17:58 +0600

    Update Contract_Revision.cpp

--- CP/beecrowd/Contract_Revision.cpp
+++ CP/beecrowd/Contract_Revision.cpp
@@ -7,29 +7,32 @@ using namespace std;
 
 int main() {
     char           x;
-    string         y, answer = "";
+    string         y;
     vector<string> results;
     while (true) {
         cin >> x >> y;
-        if (x != '0' && y != "0") {
-            for (int i = 0; i < y.size(); i++) {
-                if (y[i] != x) {
-                    answer += y[i];
-                }
-            }
-            if (stoi(answer) == 0) {
-                results.push_back("0");
-            } else {
-                results.push_back(answer);
-            }
-            answer = "";
-        } else {
+        if (x == '0' && y == "0")
             break;
-        }
+        string result = "";
+        for (char c : y)
+            if (c != x)
+                result += c;
+        int i = 0;
+        while (i < result.size() && result[i] == '0')
+            i++;
+        result = result.substr(i);
+
+        if (result == "")
+            result = "0";
+
+        results.push_back(result);
     }
-    cout << "\n\n\n\n\n\n\n answers" << endl;
+
     for (int i = 0; i < results.size(); i++) {
         cout << results[i] << endl;
     }
+
     return 0;
 }
+
+// solved
