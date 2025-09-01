cbc5e90ac7cd51f82b79f561ae3c24a2e7e73942
black <jaberhc2002@gmail.com>
2025-09-01 09:56:30 +0600

    update : modify .gitignore to include input and output files; add R_Age_in_Days.cpp and S_Interval.cpp; remove tempCodeRunnerFile.cpp; add T_Sort_Numbers.cpp

--- CP/codeforce/16-3-2025/R_Age_in_Days.cpp
+++ CP/codeforce/16-3-2025/R_Age_in_Days.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     long long time;
     cin >> time;
-    int year = time / 365;
+    int year  = time / 365;
     int month = (time % 365) / 30;
+    cout << year << " years" << endl;
+    cout << month << " months" << endl;
+    cout << (time - ((year * 365) + (month * 30))) << " days" << endl;
     return 0;
 }
+
+// solved
