3c89bb21d4dafca38e9085938a574fc7796206a5
black <jaberhc2002@gmail.com>
2025-09-04 20:54:15 +0600

    refactor: enhance T_Sort_Numbers.cpp for improved readability and structure

--- CP/codeforce/16-3-2025/T_Sort_Numbers.cpp
+++ CP/codeforce/16-3-2025/T_Sort_Numbers.cpp
@@ -1,8 +1,25 @@
 // https: // codeforces.com/group/MWSDmqGsZm/contest/219158/problem/T
 
 #include <iostream>
+#include <set>
 using namespace std;
 
 int main() {
+    long long int           x, y, z;
+    multiset<long long int> d;
+    cin >> x >> y >> z;
+    d.insert(x);
+    d.insert(y);
+    d.insert(z);
+
+    // sorted
+    for (auto a : d) {
+        cout << a << endl;
+    }
+    cout << endl;
+    // default
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
     return 0;
 }
