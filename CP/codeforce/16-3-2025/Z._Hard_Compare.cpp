7a2355f9fb2644155dae6b59e61cd76110a65d8c
jaber <jaberhc2002@gmail.com>
2025-10-04 22:09:36 +0600

    Update : z. hard fom cp

--- CP/codeforce/16-3-2025/Z._Hard_Compare.cpp
+++ CP/codeforce/16-3-2025/Z._Hard_Compare.cpp
@@ -1,22 +1,32 @@
 // https: // codeforces.com/group/MWSDmqGsZm/contest/219158/problem/Z
 
-#include <iostream>
-#include <math.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    long long a, b, c, d, res1, res2;
+#define endl '\n'
+
+void test() {
+    long long a = 5, b = 2, c = 4, d = 2;
     cin >> a >> b >> c >> d;
 
-    res1 = pow(a % 100, b % 10);
-    res2 = pow(c % 100, d % 10);
-
-    if (res1 > res2) {
+    long double x = b * log(a);
+    long double y = d * log(c);
+    if (x > y)
         cout << "YES" << endl;
-    } else if (res1 < res2) {
+    else
         cout << "NO" << endl;
-    }
+}
 
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+// freopen("error.txt", "w", stderr);
+#endif
+
+    // int t;
+    // cin >> t;
+    // while (t--)
+        test();
     return 0;
 }
