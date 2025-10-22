5ff37290c830affdf52b627cfc10b1c668e0e647
jaber <jaberhc2002@gmail.com>
2025-10-22 19:55:42 +0600

    add : some code

---
+++ projects/sorting_race/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#define vector std::vector;
+#define cout std::cout;
+#define cin std::cin;
+
+// bubble sort
+
+// insertion sort
+// selection sort
+
+int main() {
+
+    int arr[] = {21, 32,  32,  365, 123,  34122, 345, 452, 45, 76,  65, 234,
+                 21, 234, 456, 87,  6578, 7809,  34,  53,  54, 236, 648};
+
+    return 0;
+}
